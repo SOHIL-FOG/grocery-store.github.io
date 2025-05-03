e5e8a8977414da1b79a8f9cb8db80c70dd854644
SOHIL-FOG <sohilfog826@gmail.com>
2025-05-03 10:45:48 +0530

    WebTask
    grocery-store

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 6352358999 </p>
+         <p> <i class="fas fa-phone"></i> +91 9924873351 </p>
+         <p> <i class="fas fa-envelope"></i> greenhygiene92@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Rajkot, india - 360001 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit">@copyright </p>
+
+</footer>
